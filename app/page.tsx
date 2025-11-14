67c52d3db61db4fd9495305dd8930af8ed798f66
magn5310 <magnusmadsen2000@hotmail.com>
2025-11-14 10:25:23 +0100

    removed g

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Learning
+              Learnin
             </a>{" "}
             center.
           </p>
