cce50d6bb7f8eabcc2be3cde6c09e78172dca716
Marv0659 <maud@claimlane.com>
2025-12-17 13:51:58 +0100

    calender styling

--- components/ui/date-range-picker.tsx
+++ components/ui/date-range-picker.tsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar, type DateRange } from "@/components/ui/calendar"
+import { Button } from "@/components/ui/button";
+import { Calendar, type DateRange } from "@/components/ui/calendar";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { getDefaultClassNames } from "react-day-picker";
 
 interface DateRangePickerProps {
-  date?: DateRange
-  onDateChange?: (date: DateRange | undefined) => void
-  className?: string
+  date?: DateRange;
+  onDateChange?: (date: DateRange | undefined) => void;
+  className?: string;
 }
 
 export function DateRangePicker({
@@ -24,12 +24,16 @@ export function DateRangePicker({
   onDateChange,
   className,
 }: DateRangePickerProps) {
+  const defaultClassNames = getDefaultClassNames();
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
           variant={"outline"}
-          className={cn("w-full justify-start text-left font-normal", className)}
+          className={cn(
+            "w-full justify-start text-left font-normal ",
+            className
+          )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
           {date?.from ? (
@@ -48,14 +52,21 @@ export function DateRangePicker({
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
         <Calendar
-          initialFocus
           mode="range"
           defaultMonth={date?.from}
           selected={date}
           onSelect={onDateChange}
-          numberOfMonths={2}
+          numberOfMonths={1}
+          classNames={{
+            ...defaultClassNames,
+            selected: "bg-emerald-400 text-white",
+            range_start: "bg-emerald-400 text-white rounded-l-md",
+            range_end: "bg-emerald-400 text-white rounded-r-md",
+            range_middle: "bg-emerald-400/30 text-white",
+            today: "bg-emerald-400 text-white",
+          }}
         />
       </PopoverContent>
     </Popover>
-  )
+  );
 }
